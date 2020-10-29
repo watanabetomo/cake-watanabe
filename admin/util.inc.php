5f59c2ead9c6c5eb6d4dfd8762f5b5635c53b8c3
watanabe <chelseano55@gmail.com>
2020-10-29 11:55:59 +0900

    add:function description

--- admin/util.inc.php
+++ admin/util.inc.php
@@ -11,12 +11,22 @@ function h($str)
     return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
+/**
+ * ページタイトルをボタン形式で表示
+ *
+ * @return void
+ */
 function getPage()
 {
     global $title;
     echo '<h1><button type="button" class="btn title-button" disabled>' . $title . '</button></h1>';
 }
 
+/**
+ * トークン発行
+ *
+ * @return String トークン
+ */
 function getToken()
 {
     return hash('sha256', session_id());
