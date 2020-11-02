b1382d628d7c1f995375ed5981297c17b118d4e2
watanabe <chelseano55@gmail.com>
2020-11-02 12:51:28 +0900

    add: mail sender

--- purchase_done.php
+++ purchase_done.php
@@ -1,9 +1,37 @@
 <?php
 require_once('autoload.php');
+require_once('vendor/autoload.php');
+
+try {
+    $transport = new Swift_SmtpTransport(
+        SMTP_HOST,
+        SMTP_PORT,
+        SMTP_PROTOCOL
+    );
+    $transport->setUsername(GMAIL_SITE);
+    $transport->setPassword(GMAIL_APPPASS);
+    $mailer = new Swift_Mailer($transport);
+
+    $message = new Swift_Message(MAIL_TITLE);
+    $message->setFrom(MAIL_FROM);
+    $message->setTo(['chelseano55@gmail.com']);
+
+$mailBody = <<<EOT
+<h1>テスト</h1>
+EOT;
+
+    $message->setBody($mailBody, 'text/html');
+    $result = $mailer->send($message);
+} catch (Exception $e) {
+    $error = "メールの送信に失敗しました";
+}
 
 ?>
-<?php require_once('header.html')?>
+<?php require_once('header.html') ?>
 <main>
+    <?php if (isset($error)): ?>
+    <p class="error"><?=$error?></p>
+    <?php endif; ?>
     <p class="done-message">購入が完了しました。ご利用ありがとうございました。</p>
 </main>
-<?php require_once('footer.html')?>
+<?php require_once('footer.html') ?>
