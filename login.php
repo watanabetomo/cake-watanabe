18fe647b496e0645fcbb08cb0cf4e3c406aed851
watanabe <chelseano55@gmail.com>
2020-11-09 16:57:22 +0900

    fix: login page UI

--- login.php
+++ login.php
@@ -37,27 +37,32 @@ if (isset($_POST['login'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <link rel="stylesheet" href="css/login.css">
 </head>
 
 <body>
-    <h1>洋菓子店カサミンゴー 会員ログイン</h1>
-    <?php if (isset($error)): ?>
-        <p style="color: red;"><?=$error?></p>
-    <?php endif; ?>
-    <form action="" method="post">
-        <table>
-            <tr>
-                <th>ログインID</th>
-                <td><input type="text" name="id" value=""></td>
-            </tr>
-            <tr>
-                <th>パスワード</th>
-                <td><input type="password" name="pass"></td>
-            </tr>
-        </table>
-        <p><input type="submit" name="login" value="ログイン"></p>
-    </form>
+    <div class="card">
+        <div class="card-body">
+            <h1>洋菓子店カサミンゴー 会員ログイン</h1>
+            <?php if (isset($error)): ?>
+                <p style="color: red;"><?=$error?></p>
+            <?php endif; ?>
+            <form action="" method="post">
+                <table>
+                    <tr>
+                        <th>ログインID</th>
+                        <td><input type="text" name="id" value=""></td>
+                    </tr>
+                    <tr>
+                        <th>パスワード</th>
+                        <td><input type="password" name="pass"></td>
+                    </tr>
+                </table>
+                <p><input type="submit" name="login" value="ログイン"></p>
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
