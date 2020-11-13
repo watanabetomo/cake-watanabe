83f52d68e1764e01db82fc6666cb64c2257ac720
watanabe <chelseano55@gmail.com>
2020-11-13 11:53:35 +0900

    fix: mail to text

--- models/CartModel.php
+++ models/CartModel.php
@@ -138,61 +138,56 @@ class CartModel extends Model
             }
             $this->deleteFromCart();
             $mailBody =
-                '<p>' . $_SESSION['userName'] . '様</p>
-                <p>お世話になっております。<br>洋菓子店カサミンゴーカスタマーサポートです。</p>
-                <p>
-                    ' . $_SESSION['userName'] . ' 様が購入手続きをされました商品について<br>お間違えのないようメールをお送りいたしました。<br>
-                    今一度ご購入商品等にお間違えなどないよう、ご確認いただけましたら幸いでございます。
-                </p>
-                <p>--------------------------------------</p>
-                <p>【購入情報】</p>';
+                $_SESSION['userName'] . "様\n
+                お世話になっております。\n洋菓子店カサミンゴーカスタマーサポートです。\n
+                " . $_SESSION['userName'] . " 様が購入手続きをされました商品について\nお間違えのないようメールをお送りいたしました。\n
+                今一度ご購入商品等にお間違えなどないよう、ご確認いただけましたら幸いでございます。\n
+                --------------------------------------\n
+                【購入情報】\n";
             foreach ($cart as $prodOfTheCart) {
                 $productDetail = $productDetailModel->fetchById($prodOfTheCart['product_detail_id']);
                 $product = $productModel->fetchById($productDetail['product_id']);
                 $mailBody .=
-                    '<p>' . $product[0]['name'] . '<br>' . $productDetail['size'] . 'cm<br>
-                    ' . $productDetail['price'] . '円<br>
-                    ' . $prodOfTheCart['num'] . '枚</p>
-                    <p>-----------------------</p>';
+                    $product[0]['name'] . "\n" . $productDetail['size'] . "cm\n
+                    " . $productDetail['price'] . "円\n
+                    " . $prodOfTheCart['num'] . "枚\n
+                    -----------------------\n";
             }
             $mailBody .=
-                '<p>小計： ' . $_SESSION['purchase_info']['sub_price'] . '<br>
-                消費税： ' . floor($_SESSION['purchase_info']['sub_price'] * TAX) . '<br>
-                送料： ' . $_SESSION['purchase_info']['shipping'] . '<br>
-                合計： ' . $_SESSION['purchase_info']['total_price'] . '</p>
-                <p>--------------------------------------</p>
-                <p>【送付先情報】</p>
-                <p>お名前： ' . $_SESSION['purchase_info']['name'] . '<br>
-                フリガナ： ' . $_SESSION['purchase_info']['name_kana'] . '<br>
-                電話番号： ' . $_SESSION['purchase_info']['tel1'] . ' - ' . $_SESSION['purchase_info']['tel2'] . ' - ' . $_SESSION['purchase_info']['tel3'] . '<br>
-                郵便番号： ' . $_SESSION['purchase_info']['postal_code1'] . ' - ' . $_SESSION['purchase_info']['postal_code2'] . '<br>
-                都道府県： ' . $_SESSION['purchase_info']['pref'] . '<br>
-                市区町村： ' . $_SESSION['purchase_info']['city'] . '<br>
-                番地： ' . $_SESSION['purchase_info']['address'] . '<br>
-                マンション名等： ' . $_SESSION['purchase_info']['other'] . '</p>
-                <p>--------------------------------------</p>
-                <p>【請求先情報】</p>
-                <p>お名前： ' . $user['name'] . '<br>
-                フリガナ： ' . $user['name_kana'] . '<br>
-                電話番号： ' . $user['tel1'] . ' - ' . $user['tel2'] . ' - ' . $user['tel3'] . '<br>
-                郵便番号： ' . $user['postal_code1'] . ' - ' . $user['postal_code2'] . '<br>
-                都道府県： ' . $prefectures[$user['pref']] . '<br>
-                市区町村： ' . $user['city'] . '<br>
-                番地： ' . $user['address'] . '<br>
-                マンション名等： ' . $user['other'] . '</p>
-                <p>--------------------------------------</p>
-                <p>商品ご到着まで。今しばらくお待ちください。</p>
-                <p>※このメールは自動送信メールです。<br>※返信をされてもご回答しかねますのでご了承ください。</p>';
-            $from = 'From:' . mb_encode_mimeheader('洋菓子店カサミンゴー');
-            $from .= "\r\n";
-            $from .= 'Content-type: text/html; charset=UTF-8';
+                "小計： " . $_SESSION['purchase_info']['sub_price'] . "\n
+                消費税： " . floor($_SESSION['purchase_info']['sub_price'] * TAX) . "\n
+                送料： " . $_SESSION['purchase_info']['shipping'] . "\n
+                合計： " . $_SESSION['purchase_info']['total_price'] . "\n
+                --------------------------------------\n
+                【送付先情報】\n
+                お名前： " . $_SESSION['purchase_info']['name'] . "\n
+                フリガナ： " . $_SESSION['purchase_info']['name_kana'] . "\n
+                電話番号： " . $_SESSION['purchase_info']['tel1'] . ' - ' . $_SESSION['purchase_info']['tel2'] . ' - ' . $_SESSION['purchase_info']['tel3'] . "\n
+                郵便番号： " . $_SESSION['purchase_info']['postal_code1'] . ' - ' . $_SESSION['purchase_info']['postal_code2'] . "\n
+                都道府県： " . $_SESSION['purchase_info']['pref'] . "\n
+                市区町村： " . $_SESSION['purchase_info']['city'] . "\n
+                番地： " . $_SESSION['purchase_info']['address'] . "\n
+                マンション名等： " . $_SESSION['purchase_info']['other'] . "\n
+                --------------------------------------\n
+                【請求先情報】\n
+                お名前： " . $user['name'] . "\n
+                フリガナ： " . $user['name_kana'] . "\n
+                電話番号： " . $user['tel1'] . ' - ' . $user['tel2'] . ' - ' . $user['tel3'] . "\n
+                郵便番号： " . $user['postal_code1'] . ' - ' . $user['postal_code2'] . "\n
+                都道府県： " . $prefectures[$user['pref']] . "\n
+                市区町村： " . $user['city'] . "\n
+                番地： " . $user['address'] . "\n
+                マンション名等： " . $user['other'] . "\n
+                --------------------------------------\n
+                商品ご到着まで。今しばらくお待ちください。\n
+                ※このメールは自動送信メールです。\n※返信をされてもご回答しかねますのでご了承ください。";
             mb_language('uni');
             mb_internal_encoding('UTF-8');
             if (!mb_send_mail(
                 $_SESSION['purchase_info']['mail'],
                 '【洋菓子店カサミンゴー】ご購入商品確認メール',
                 $mailBody,
-                $from
+                'From:' . mb_encode_mimeheader('洋菓子店カサミンゴー')
             )) {
                 throw new Exception;
             }
