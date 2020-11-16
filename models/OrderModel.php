fda2639c3580450cd5edf837331bbd7ac36af60a
watanabe <chelseano55@gmail.com>
2020-11-16 12:58:41 +0900

    refactoring

--- models/OrderModel.php
+++ models/OrderModel.php
@@ -28,7 +28,7 @@ class OrderModel extends Model
     public function commitOrder( $user_id,$name,$name_kana,$mail,$tel1,$tel2,$tel3,$postal_code1,$postal_code2,$pref,$city,$address,$other,$payment_id,$sub_price,$shipping_price,$tax,$total_price,$dbh)
     {
         $stmt = $dbh->prepare('INSERT INTO `order`(user_id, name, name_kana, mail, tel1, tel2, tel3, postal_code1, postal_code2, pref, city, address, other, payment_id, sub_price, shipping_price, tax, total_price) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
-        $stmt->execute([$user_id, $name, $name_kana, $mail, $tel1, $tel2, $tel3, $postal_code1, $postal_code2, $pref, $city, $address, $other, $payment_id, $sub_price, $shipping_price, $tax, $total_price]);
+        $stmt->execute([$user_id, $name, $name_kana, $mail, $tel1, $tel2, $tel3, $postal_code1, $postal_code2, $pref, $city, $address, ($other == '' ? null : $other), $payment_id, $sub_price, $shipping_price, $tax, $total_price]);
     }
 
     /**
