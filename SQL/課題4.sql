a45da1a52fa923f3b036f49696d56971afae5a47
watanabe <chelseano55@gmail.com>
2020-11-27 15:45:54 +0900

    refactoring

--- SQL/課題4.sql
+++ SQL/課題4.sql
@@ -1,7 +1,8 @@
 新規テーブル（在庫情報）：stock
 id 在庫情報id（主キー）
 product_detail_id 商品詳細id
-num 在庫数
+actual_num 実在庫数
+unsettled_num 注文未確定在庫数
 name 商品名
 size サイズ
 price 価格
@@ -12,7 +13,8 @@ status 購入・確定・キャンセルの各状態を保持(1:購入 2:確定 
 CREATE TABLE stock (
     id SERIAL PRIMARY KEY,
     product_detail_id BIGINT(20) UNSIGNED NOT NULL,
-    num SMALLINT UNSIGNED NOT NULL,
+    actual_num SMALLINT UNSIGNED NOT NULL,
+    unsettled_num SMALLINT UNSIGNED NULL DEFAULT NULL,
     name TEXT NOT NULL,
     size SMALLINT UNSIGNED NOT NULL,
     price INT UNSIGNED NOT NULL,
@@ -21,5 +23,5 @@ CREATE TABLE stock (
 ALTER TABLE `order`
 ADD status SMALLINT UNSIGNED NOT NULL;
 
-INSERT INTO stock (product_detail_id, num, name, size, price)
+INSERT INTO stock (product_detail_id, actual_num, name, size, price)
 VALUES (1, 1000, 'チョコレートケーキ', 50, 1000);
